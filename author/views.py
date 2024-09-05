e649392b74f07687d4b2955812f638c770bc2b28
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2024-09-05 12:59:37 +0600

    redirect link added

--- author/views.py
+++ author/views.py
@@ -41,9 +41,9 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active = True
         user.save()
-        return redirect('login')
+        return redirect('https://66d95488a7edd4c29170daaa--friendly-bombolone-93aaf5.netlify.app/login')
     else:
-        return redirect('register')
+        return redirect('https://66d95488a7edd4c29170daaa--friendly-bombolone-93aaf5.netlify.app/register')
 
 class AuthorLoginApiView(APIView):
     serializer_class = AuthorLoginSerializer
