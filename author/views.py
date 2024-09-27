b36296728c9744a2d75c554d79ee5e30d77e1cda
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2024-09-28 00:07:03 +0600

    changed api link

--- author/views.py
+++ author/views.py
@@ -41,9 +41,9 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active = True
         user.save()
-        return redirect('https://66d95488a7edd4c29170daaa--friendly-bombolone-93aaf5.netlify.app/login')
+        return redirect('https://ph-cricket-scorer.netlify.app/login')
     else:
-        return redirect('https://66d95488a7edd4c29170daaa--friendly-bombolone-93aaf5.netlify.app/register')
+        return redirect('https://ph-cricket-scorer.netlify.app/register')
 
 class AuthorLoginApiView(APIView):
     serializer_class = AuthorLoginSerializer
