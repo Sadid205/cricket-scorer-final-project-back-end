561bb4074445a164bdcddfe978c6ed135d601b6b
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-01-06 18:35:28 +0600

    added https://ph-cricket-scorer.netlify.app/ into allowed host

--- cricketscorer/settings.py
+++ cricketscorer/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-3!80tgmb%3958ho*q&l%gmqr-0q6-1oy#zslt%hs+oezn#h!ab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cricketscorer-g4qfahsta-sadid205s-projects.vercel.app','localhost','127.0.0.1','frontend']
-CSRF_TRUSTED_ORIGINS = ['cricketscorer-g4qfahsta-sadid205s-projects.vercel.app','https://*.127.0.0.1']
+ALLOWED_HOSTS = ['https://ph-cricket-scorer.netlify.app/','localhost','127.0.0.1','frontend']
+CSRF_TRUSTED_ORIGINS = ['https://ph-cricket-scorer.netlify.app/','https://*.127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = False
 
 # Application definition
